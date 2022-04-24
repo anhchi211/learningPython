d9569b893d6ed517ade2518fbf7f5f5984c1bad1
MAC <mac@MACs-MacBook-Air.local>
2022-04-24 14:28:56 +0300

    Updated

---
+++ Baby steps/integers&floats.py
@@ -0,0 +1,21 @@
+#Data types: Numbers
+ # Integer numbers: decimal, binary, octal, hexadecimal
+ # Floating numbers
+ # Complex numbers
+
+#Integer 
+a = 56
+b = 80
+print (a+b)
+print(a/b)
+
+#Float
+c = 14.686
+d = 15.123
+print (a+c)
+
+#String and Number
+string = 'python is fun'
+print(string +  str(d))
+
+
