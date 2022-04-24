70d3c82ba1473ba8f66012edb5dea2fa83e48b67
MAC <mac@MACs-MacBook-Air.local>
2022-04-24 14:54:18 +0300

    update

---
+++ Baby steps/inputs.py
@@ -0,0 +1,4 @@
+lastname=input('Type your lastname: ')
+firstname=input('Tyep you firstname: ')
+greeting= "Hello" + " " + lastname  + " " + firstname
+print(greeting)
--- Baby steps/strings.py
+++ Baby steps/strings.py
@@ -1,6 +1,8 @@
 # Blacklash (\) character rules for string
    # (\) use as the last character of a line -> indicate the next line is a continuation
    # (\n\) (\n) use as the last character of a line -> to make the string contain two lines
+# \t - Tab
+
 
 myname = "Nguyen Diep \nAnh Chi"
 print(myname) 
@@ -20,11 +22,19 @@ print (myfullname.lower())
 print (myfullname.islower()) 
 print (myfullname.isupper())
 
-#len function -> How many characters included in the string?
+#len function -> How many characters(index) included in the string?
 print(len(myfullname))
+
+#index 
+print(myfullname[2:8]) 
 
 #replace function
 print(myfullname.replace("Nguyen", "Do"))
+
+
+
+
+
 
 
 
